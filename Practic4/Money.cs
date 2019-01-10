b6700b27c948c59484cd1e8e3a71fdea121395cf
DenisShulyak <denshufly@mail.ru>
2019-01-10 22:20:16 +0600

    PracticWork4

---
+++ Practic4/Money.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic4
+{
+    class Money
+    {
+
+
+        private int _NumberBill; // Номинал купюры
+        private int _countBills; // Кол-во купюр
+        private int _Sum; // Общее кол-во денежных средств
+        public Money()
+        {
+        }
+        public Money(int NumberBill,int countBills)
+        {
+            _NumberBill = NumberBill;
+            _countBills = countBills;
+            _Sum = _NumberBill * _countBills;
+
+        }
+        public int GetNumberBill()
+        {
+            return _NumberBill;
+        }
+        public void SetNumberBill(int NumberBill)
+        {
+
+            _NumberBill = NumberBill;
+        }
+
+        public int GetCountBills()
+        {
+            return _countBills;
+        }
+        public void SetCountBills(int countBills)
+        {
+
+            _countBills = countBills;
+        }
+        public bool IsThereEnoughMoney(int PriceProduct)
+        {
+            if(PriceProduct <=_Sum)
+            {
+            return true;
+            }
+            return false;
+        }
+        public int CanBuyProducts(int PriceProduct)
+        {
+            return _Sum / PriceProduct;
+        }
+
+        public int NumberBill
+        {
+            get => _NumberBill; 
+            set =>_NumberBill = value; 
+        }
+        public int CountBills
+        {
+            get =>_countBills; 
+            set =>_countBills = value; 
+        }
+        public int Sum
+        {
+            get => _Sum; 
+        }
+    }
+}
---
+++ Practic4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberBill = 1000;
+            int countBills = 4;
+            Money money = new Money(numberBill, countBills);
+            Console.WriteLine("Номинал купюры: " + money.GetNumberBill());
+            Console.WriteLine("Количество купюр: " + money.GetCountBills());
+            Console.WriteLine("Введите цену товара: ");
+            int PriceProduct1 = int.Parse(Console.ReadLine());
+            if(money.IsThereEnoughMoney(PriceProduct1) == true)
+            {
+                Console.WriteLine("Денежных средств хватит на покупку товара");
+            }
+            else
+            {
+                Console.WriteLine("Денежных средств не хватит на покупку товара");
+            }
+            Console.WriteLine("Введите цену товара(n): ");
+            int PriceProduct2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Количество товаров, которые можно купить за нашие денежные средства: " + money.CanBuyProducts(PriceProduct2));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
